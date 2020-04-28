5d60822ac0e3f1f54d4e78b7b19fd8949384719e
Felix <felix.murray@gmail.com>
2020-04-28 02:27:58 -0700

    [wip] Add Sign In Widget.

---
+++ src/2d-Graphics-Modler/signin.h
@@ -0,0 +1,25 @@
+#ifndef SIGNIN_H
+#define SIGNIN_H
+
+#include <QWidget>
+
+namespace Ui {
+class SignIn;
+}
+
+class SignIn : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SignIn(QWidget *parent = nullptr);
+    ~SignIn();
+
+private slots:
+    void on_pushButton_signin_clicked();
+
+private:
+    Ui::SignIn *ui;
+};
+
+#endif // SIGNIN_H
