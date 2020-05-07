b70db7aa95777e3791d953f7bac66d80b878e520
Felix <felix.murray@gmail.com>
2020-05-06 18:15:36 -0700

    [feat] Add Line shape implementation, modify header.

--- src/2d-Graphics-Modler/line.cpp
+++ src/2d-Graphics-Modler/line.cpp
@@ -1,36 +1,28 @@
 #include "line.h"
 
-Line::Line(QPaintDevice *device, int id)
+Line::Line(QPaintDevice *device, int id) : Shape(device, id, Shape::shape), line_begin(1,1), line_end(1,1)
 {
-//...
-}
-
-Line::~Line()
-{
-//...
-}
-
-void Line::setPoints(QPoint x, QPoint y)
-{
-//...
+    setShape(LineObj);
 }
 
 void Line::move(const int tX, const int tY, int ptIndex)
 {
-//...
+ QPoint temp(tX, tY);
+    switch(ptIndex)
+    {
+        case 1: line_begin = temp;
+            break;
+        case 2:   line_end = temp;
+            break;
+        default: {}
+    }
 }
 
 void Line::draw(QPaintDevice *device)
 {
-//...
-}
-
-double Line::perimeter()
-{
-//...
-}
-
-double Line::area()
-{
-//...
+    painter.begin(device);
+    painter.setPen(getPen());
+    painter.setBrush(getBrush());
+    painter.drawLine(line_begin, line_end);
+    painter.end();
 }
