0b0ee3a4e045d910a6f1a9317a7f2dade4609a9c
oggunderscore <oggunderscore@gmail.com>
2020-05-08 15:16:15 -0700

    [feat] Ellipse Documentation Implemented

--- src/2d-Graphics-Modler/ellipse.h
+++ src/2d-Graphics-Modler/ellipse.h
@@ -6,27 +6,37 @@
 #include <QPaintDevice>
 #include <QPainter>
 
+// Class Definition for Class Ellipse
 class Ellipse : public Shape
 {
 public:
+    
+    // Constructor using QPaintDevice
     Ellipse(QPaintDevice* device = nullptr, int id = -1);
+
+    // Default destructor
     ~Ellipse() override {}
 
+    // Class functions
     void move(int x, int y, int na) override;
     void draw(QPaintDevice* device) override;
-
+    
+    // Functions to calculate area and perimeter
     double area() override;
     double perimeter() override;
 
+    // Getter Functions
     double getWidth();
     double getHeight();
     QPoint& getLocation();
 
+    // Setter Functions
     void setDimensions(double w, double h);
     void setLocation(int x, int y);
     void setLocation(QPoint pt);
 
 private:
+    // Class Vars
     double width;
     double height;
     QPoint location;
